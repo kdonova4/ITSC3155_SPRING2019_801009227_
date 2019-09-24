4636aca520e0430f67534c82a3aa8215992ff4dd
Kevin <kdonova4@uncc.edu>
2019-09-24 14:47:15 +0000

    Ruby OO Basics – Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -3,8 +3,17 @@
 class BookInStock
     
     def initialize(isbn, price)
+        unless isbn.size > 0
+            raise ArgumentError.new("No empty strings")
+        end
+        unless price > 0
+        raise ArgumentError.new("needs to be bigger than 0")
+            
+        end
     @isbn = isbn
     @price = price
     end
     attr_accessor :isbn, :price
+    
+    
 end
