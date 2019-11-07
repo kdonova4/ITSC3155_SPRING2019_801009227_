70354830de511990dd25a68d77795cb6cce038a7
Kevin <kdonova4@uncc.edu>
2019-11-07 15:59:18 +0000

    Add blogger adds an article scenario 1

---
+++ blog/spec/integration/blogger_adds_an_article_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper.rb'
+
+
+feature"Blogger adds an article" do
+    scenario "Blogger succesfully navigates to the new articles page from the listing articles page" do
+        visit articles_path
+        expect(page).to have_content("Listing articles")
+        click_link "New article"
+        expect(page).to have_content("New Article")
+        expect(page).to have_field("Title")
+        expect(page).to have_field("Text")
+    end
+        
+        
+    
+end
